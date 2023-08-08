ed5e0d7fe052ca3cf40164fe924e0cf441ab96d2
2weiEmu <saalbach.robert@outlook.de>
2023-08-08 19:43:08 +0200

    added some text fade in, things like that

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,5 +1,14 @@
 function toggleMenu() {
-        var sideMenu = document.getElementById("side-menu");
-	sideMenu.style.display = sideMenu.style.display != "flex" ? "flex" : "none"
+    var sideMenu = document.getElementById("side-menu");
+    sideMenu.style.display = sideMenu.style.display != "flex" ? "flex" : "none";
 }
 
+
+window.onload = async function() {
+    var robertName = "Robert Arno Saalbach";
+    var mainName = document.getElementById("main-name");
+    for (var i = 0; i < robertName.length; i++) {
+        mainName.textContent += robertName[i];
+        await new Promise(r => setTimeout(r, 60));
+    }
+}
