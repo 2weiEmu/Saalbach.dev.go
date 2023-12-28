b330161596c77497a7f7a4b6d483ff3c6f7a2cb0
2weiEmu <saalbach.robert@outlook.de>
2023-12-28 21:16:39 +0100

    another rework, a simple design. Using Mux Router

--- src/main.go
+++ src/main.go
@@ -1,139 +1,14 @@
 package main
 
 import (
-	"encoding/csv"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"regexp"
 	"strconv"
-	"strings"
-	"text/template"
+
+	"github.com/gorilla/mux"
 )
 
-type BlogItem struct {
-    Path string
-    Title string 
-    Tags []string
-    Author string
-    Date string
-    Desc string
-    Content string
-}
-
-var blogheaders []BlogItem
-
-func GetHeaders(filter string) []BlogItem {
-    var result []BlogItem
-    
-    f, err := os.Open("src/bloghead.csv")
-    if err != nil {
-        // TODO:
-    }
-    defer f.Close()
-
-    csvReader := csv.NewReader(f)
-    data, err := csvReader.ReadAll()
-    if err != nil {
-        // TODO:
-    }
-
-    // converting the data array to blogItems
-    // data is [][]string
-    for _, line := range data {
-        
-        if m, _ := regexp.MatchString("(?i)" + filter, line[1]); m {
-            var item BlogItem
-            item.Path = line[0]
-            item.Title = line[1]
-            item.Author = line[2]
-            item.Date = line[3]
-            item.Desc = line[4]
-            for i := 5; i < len(line); i++ {
-                item.Tags = append(item.Tags, line[i])
-            }
-            result = append(result, item)
-        }
-
-    }
-    return result
-}
-
-func LoadContentFromPath(path string) string {
-    c, err := os.ReadFile("src/static/blogs/" + path + ".html")
-    if err != nil {
-        // TODO:
-    }
-
-    return string(c)
-
-}
-
-func RouteHandler(writer http.ResponseWriter, request *http.Request) {
-    requestPath := request.URL.Path
-    fmt.Println("Request to: ", requestPath)
-
-    titleFilter := request.URL.Query().Get("blogfilter")
-    
-    // Serving the main page
-    if requestPath == "/" {
-        blogheaders = GetHeaders(titleFilter)
-        index, err := template.ParseFiles("src/static/templates/index.html")
-        if err != nil {
-            // TODO:
-        }
-        index.Execute(writer, blogheaders)
-
-    // Serving static files
-    } else if match, _ := regexp.MatchString("^/((css)|(js))/", requestPath); match {
-        fs := http.FileServer(http.Dir("src/static"))
-        fs.ServeHTTP(writer, request)
-
-    // Serving any blog
-    } else if match, _ := regexp.MatchString("^/blogs/[^/]", requestPath); match {
-        blog, err := template.ParseFiles("src/static/templates/blog.html")
-        if err != nil {
-            // TODO:
-        }
-
-        path, _ := strings.CutPrefix(requestPath, "/blogs/")
-        content := LoadContentFromPath(path)
-
-        var finalHeader BlogItem
-        blogheaders := GetHeaders("")
-        for _, header := range blogheaders {
-            if header.Path == path {
-                finalHeader = header
-            }
-        }
-
-        finalHeader.Content = content
-
-        blog.Execute(writer, finalHeader)
-
-    // Serving any images
-    } else if match, _ := regexp.MatchString("^/images/", requestPath); match {
-        fs := http.FileServer(http.Dir("src/static"))
-        http.StripPrefix("/static", fs)
-        fs.ServeHTTP(writer, request)
-
-    // Serving the 404 page
-    } else {
-        http.ServeFile(writer, request, "src/static/templates/404.html")
-    }
-}
-
-func httpRedirect(w http.ResponseWriter, req *http.Request) {
-    // remove/add not default ports from req.Host
-    target := "https://" + req.Host + req.URL.Path 
-    if len(req.URL.RawQuery) > 0 {
-        target += "?" + req.URL.RawQuery
-    }
-    log.Printf("redirect to: %s", target)
-    http.Redirect(w, req, target, http.StatusPermanentRedirect)
-}
 
 func main() {
     /**
@@ -147,7 +22,13 @@ func main() {
     flag.Parse()
 
     // must give deploy or test - otherwise invalid
-    http.HandleFunc("/", RouteHandler)
+    r := mux.NewRouter()
+    r.HandleFunc("/css/{style}", CSSHandler)
+    r.HandleFunc("/images/{image}", ImagesHandler)
+    r.HandleFunc("/blogs/{blog}", BlogHandler)
+    r.HandleFunc("/", MainHandler)
+    http.Handle("/", r)
+
     fmt.Println("Server is almost ready.")
 
     var err error
@@ -155,7 +36,7 @@ func main() {
     if !deploy {
         err = http.ListenAndServe(":" + port, nil)
     } else {
-        go http.ListenAndServe(":80", http.HandlerFunc(httpRedirect))
+        go http.ListenAndServe(":80", nil)
         err = http.ListenAndServeTLS(":" + port, cert, secret, nil)
     }
 
@@ -164,3 +45,22 @@ func main() {
     }
 }
 
+func CSSHandler(writer http.ResponseWriter, request *http.Request) {
+    vars := mux.Vars(request)
+    style := vars["style"]
+    http.ServeFile(writer, request, "src/static/css/" + style);
+}
+
+func BlogHandler(writer http.ResponseWriter, request *http.Request) {
+    vars := mux.Vars(request)
+    blog := vars["blog"]
+    http.ServeFile(writer, request, "src/static/blogs/" + blog);
+}
+
+func ImagesHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func MainHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
--- src/static/blogs/my-uni.md
+++ src/static/blogs/my-uni.md
@@ -1,15 +1,2 @@
-# Welcome to my Blogs
-
-- Hi 
-- This is a list of things that I like
-
-Oh yea, also this exists.
-
-I have a question
-
-## What question?
-
-Uh huh
 
 
-# This is a header
